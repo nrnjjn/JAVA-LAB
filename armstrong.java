68fef09313912d0d64d5f57d6e7b0833a09a60a6
Niranjan Babu <71537877+nrnjjn@users.noreply.github.com>
2023-03-16 02:58:10 -0700

    Add files via upload

---
+++ armstrong.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Arm
+{
+public static void main(String args[])
+{
+Scanner input=new Scanner(System.in);
+int number,temp,total=0;
+System.out.println("Enter the number");
+int num=input.nextInt();
+number=num;
+while(number!=0)
+{
+temp=number%10;
+total=total+temp*temp*temp;
+number=number/10;
+}
+if(total==num)
+System.out.println("Armstrong number");
+else
+System.out.println("Not armstrong");
+}
+}
+
